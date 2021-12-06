666b72ec7a7f3c63f4c10303958681ccf85581a2
Jari Honkanen <jaarhonka@yahoo.com>
2021-12-06 22:19:32 +0200

    update uart_pkg.vhd

--- uart_pkg.vhd
+++ uart_pkg.vhd
@@ -38,6 +38,8 @@ package uart_pkg is
     end component uart;
     
     -- signal uart_clocks   : uart_clock_group;
+    -- signal uart_FPGA_in  : uart_FPGA_input_group;
+    -- signal uart_FPGA_out : uart_FPGA_output_group;
     -- signal uart_data_in  : uart_data_input_group;
     -- signal uart_data_out : uart_data_output_group;
     
