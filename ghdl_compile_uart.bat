dcdeeaf6df852f3d36ae7f35c2d47bbb283a24d5
Jari Honkanen <jaarhonka@yahoo.com>
2023-06-29 11:52:22 +0300

    add paramter for calling with submodule path

--- ghdl_compile_uart.bat
+++ ghdl_compile_uart.bat
@@ -1,9 +1,5 @@
 echo off
-echo %project_root%
-FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse --show-toplevel`) DO (
-SET project_root=%%F
-)
-SET source=%project_root%/../
 
-ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_uart/uart_tx/uart_tx_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_uart/uart_rx/uart_rx_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %1/uart_tx/uart_tx_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %1/uart_rx/uart_rx_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %1/uart_protocol/uart_protocol_pkg.vhd
